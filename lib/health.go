fab82c17a65b650d64716f1af7a1bfc8d6dd2b89
Eno Compton <eno4@ecom.com>
2017-11-25 19:39:34 -0700

    Add guid to /healthz

--- lib/health.go
+++ lib/health.go
@@ -2,12 +2,23 @@ package random
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
 )
 
 // NewHealthHandler is the constructor for HealthHandler.
 func NewHealthHandler(version string) *HealthHandler {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatalf("Failed to generate Health guid: %v", err)
+	}
+	guid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+
 	return &HealthHandler{
+		guid:    guid,
 		version: version,
 	}
 }
@@ -15,13 +26,17 @@ func NewHealthHandler(version string) *HealthHandler {
 // HealthHandler provides a means to report on the health of the running web
 // application. It reports the application's version.
 type HealthHandler struct {
+	guid    string
 	version string
 }
 
 // ServeHTTP will return the applicaton's version as JSON.
 func (h *HealthHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Add("content-type", "application/json; charset=utf-8")
-	info := HealthInfo{Version: h.version}
+	info := HealthInfo{
+		GUID:    h.guid,
+		Version: h.version,
+	}
 	encoder := json.NewEncoder(rw)
 	err := encoder.Encode(info)
 	if err != nil {
@@ -32,5 +47,6 @@ func (h *HealthHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 // HealthInfo represents a summary of the appliation's status.
 type HealthInfo struct {
+	GUID    string `json:"guid"`
 	Version string `json:"version"`
 }
