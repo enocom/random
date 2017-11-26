f733d8468f33f1fc3f8a70919f91e158d184f4c8
Eno Compton <eno4@ecom.com>
2017-11-25 17:31:45 -0700

    Scale to device width

--- lib/handler.go
+++ lib/handler.go
@@ -40,6 +40,7 @@ const (
 	indexFormat = `<html>
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>random</title>
 	<style>
 		.swatch {
